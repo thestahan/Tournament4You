a0b58f6bca97ec6e5ef83e404b06d8fc36d5c10c
Kacper <thestahan@gmail.com>
2022-03-30 19:33:09 +0200

    71 - Add vertical slice to login user

---
+++ API/Features/Users/Login.cs
@@ -0,0 +1,61 @@
+﻿using API.Domain;
+using API.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Features.Users;
+
+public class Login
+{
+    public class Command : IRequest<Result>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Result
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string TokenExpiration { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Command, Result>
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
+
+        public Handler(UserManager<AppUser> userManager,
+                       ITokenService tokenService)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            if (user is null)
+            {
+                throw new BadHttpRequestException("Login credentials are invalid.");
+            }
+
+            var result = await _userManager.CheckPasswordAsync(user, request.Password);
+
+            if (result is false)
+            {
+                throw new BadHttpRequestException("Login credentials are invalid.");
+            }
+
+            var jwt = _tokenService.CreateToken(user);
+
+            return new Result
+            {
+                Email = request.Email,
+                Token = jwt,
+                TokenExpiration = "86400"
+            };
+        }
+    }
+}
