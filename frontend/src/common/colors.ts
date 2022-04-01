e98f4749f0b3a1e042857dcc7722ac2674f3b6fd
Maciej Kulinski <mafciej99@gmail.com>
2022-04-01 12:35:14 +0200

    Basic setup

--- frontend/src/common/colors.ts
+++ frontend/src/common/colors.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   white: "#FFFFFF",
   black: "#000000",
--- frontend/src/common/ui/buttons.tsx
+++ frontend/src/common/ui/buttons.tsx
@@ -6,16 +6,26 @@ type Props = {
   backgroundColor?: keyof typeof colors;
 };
 
-export const Button = styled.div<Props>`
+export const StyledButton = styled.button<Props>`
+  font-family: inherit;
+  font-size: 100%;
+  line-height: 1.15;
+  margin: 0;
+  text-transform: none;
+  -webkit-appearance: button;
+  overflow: visible;
+  border: none;
+  cursor: pointer;
+
   width: fit-content;
-  padding: 10px 5px;
+  padding: 10px 30px;
   text-align: center;
   background-color: ${(props: Props) =>
     colors[props.backgroundColor || "white"]};
   color: ${(props: Props) => colors[props.color || "black"]};
 `;
 
-export const PrimaryButton = styled(Button)`
+export const Button = styled(StyledButton)`
   background-color: ${({ backgroundColor }) =>
     backgroundColor ? colors[backgroundColor] : "white"};
 `;
