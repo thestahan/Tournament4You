05f711406e8184874d5fad5becdd301212e81d96
Maciej Kulinski <mafciej99@gmail.com>
2022-04-01 12:35:14 +0200

    Basic setup

--- frontend/src/common/ui/inputs.tsx
+++ frontend/src/common/ui/inputs.tsx
@@ -1,17 +1,66 @@
 import styled from "@emotion/styled";
 import React from "react";
+import { InputInterface } from "common/models";
+import { useState } from "react";
 
-export const Input = styled.input<{ incorrect?: boolean }>`
-  border: 1px solid black;
-  border-radius: 2px;
-  padding: 10px 5px;
+export const Container = styled.div`
+  display: flex;
 `;
 
-export const InputLabel = styled.div``;
+export const StyledInput = styled.input<{ incorrect?: boolean }>`
+  border: none;
+  border-bottom: 1px solid black;
+  padding: 10px 5px 10px 0px;
+  position: relative;
+  width: 200px;
 
-export const LabeledInput: React.FC<{ label?: string }> = ({ label }) => (
-  <div>
-    <InputLabel>{label}</InputLabel>
-    <Input />
-  </div>
-);
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const Icon = styled.i`
+  right: 0;
+  position: relative;
+  margin-left: -20px;
+  margin-top: 10px;
+  cursor: pointer;
+`;
+
+export const Input: React.FC<InputInterface> = ({ label, value, setValue }) => {
+  return (
+    <Container>
+      <StyledInput
+        placeholder={label}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </Container>
+  );
+};
+
+export const PasswordInput: React.FC<InputInterface> = ({
+  label,
+  value,
+  setValue,
+}) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  return (
+    <Container>
+      <StyledInput
+        type={showPassword ? "text" : "password"}
+        placeholder={label}
+        style={{
+          paddingRight: "25px",
+        }}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Icon
+        className="pi pi-eye mr-2"
+        onClick={() => setShowPassword(!showPassword)}
+      ></Icon>
+    </Container>
+  );
+};
--- frontend/src/dashboard/dashboard-route.tsx
+++ frontend/src/dashboard/dashboard-route.tsx
@@ -1,5 +1,43 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+import * as ui from "common/ui";
+
+const Container = styled.div`
+  height: 100%;
+`;
+
+const Header = styled.div`
+  margin-bottom: 20px;
+`;
+
 const DashboardRoute = () => {
-  return <></>;
+  const [currentUser, setCurrentUser] = useState(false);
+
+  const [firstname, setFirstname] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Container>
+      {currentUser ? (
+        <div>Jestem zalogowany</div>
+      ) : (
+        <div>
+          <Header>Nie jestem zalogowany</Header>
+          <br />
+          <ui.Input
+            label="Firstname"
+            value={firstname}
+            setValue={setFirstname}
+          ></ui.Input>
+          <ui.PasswordInput
+            label="Password"
+            value={password}
+            setValue={setPassword}
+          ></ui.PasswordInput>
+        </div>
+      )}
+    </Container>
+  );
 };
 
 export default DashboardRoute;
