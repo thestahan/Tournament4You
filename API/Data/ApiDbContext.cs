ff758970f8d2b86f3cdc464b200f644e515bb6d0
Kamil Eł <kamillancuchowski@gmail.com>
2022-04-03 15:17:44 +0200

    5 - API - Add DbSet

--- API/Data/ApiDbContext.cs
+++ API/Data/ApiDbContext.cs
@@ -9,9 +9,17 @@ namespace API.Data
         public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
         {
         }
+        public DbSet<Match> Matches => Set<Match>();
+        public DbSet<Player> Players => Set<Player>();
+        public DbSet<Position> Positions => Set<Position>();
+        public DbSet<Team> Teams => Set<Team>();
+        public DbSet<Tournament> Tournaments => Set<Tournament>();
+
+
+
 
         protected override void OnModelCreating(ModelBuilder builder)
-{
+        {
             base.OnModelCreating(builder);
         }
     }
