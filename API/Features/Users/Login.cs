216036078287b1f924dd8bf00d636c631b03effe
Kacper <thestahan@gmail.com>
2022-04-05 16:32:36 +0200

    77 - API - Add tests cases covering user login

--- API/Features/Users/Login.cs
+++ API/Features/Users/Login.cs
@@ -58,14 +58,14 @@ public class Login
                 TokenExpiration = "86400"
             };
         }
+    }
 
-        public class CommandValidator : AbstractValidator<Command>
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
         {
-            public CommandValidator()
-            {
-                RuleFor(c => c.Email).NotEmpty().EmailAddress();
-                RuleFor(c => c.Password).NotEmpty().MinimumLength(8);
-            }
+            RuleFor(c => c.Email).NotEmpty().EmailAddress();
+            RuleFor(c => c.Password).NotEmpty().MinimumLength(8);
         }
     }
 }
