c414ed7b2c67b02b9a7d9f24fe701d3774a9e6c6
Tomasz Kowalski <t.kowalski1608@gmail.com>
2022-04-08 10:36:36 +0200

    Initial dashboard view
    :wq

--- frontend/src/common/images/index.ts
+++ frontend/src/common/images/index.ts
@@ -1,2 +1,5 @@
 export { default as LogoBlack } from "./logo-black.png";
 export { default as LogoWhite } from "./logo-white.png";
+export { default as VoleyballTournamentLogo } from "./voleyball-tournament-logo.png";
+export { default as VoleyballPlayerLeft } from "./voleyball-player-left.png";
+export { default as VoleyballPlayerRight } from "./voleyball-player-right.png";
