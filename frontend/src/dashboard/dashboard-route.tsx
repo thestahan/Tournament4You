89bf6a3b6539a61a67f1648860ddbc63874d77bc
Tomasz Kowalski <t.kowalski1608@gmail.com>
2022-04-08 10:36:36 +0200

    Initial dashboard view
    :wq

--- frontend/src/dashboard/dashboard-route.tsx
+++ frontend/src/dashboard/dashboard-route.tsx
@@ -1,12 +1,34 @@
 import styled from "@emotion/styled";
 import { isAuthenticated } from "common/api/utils/local-storage";
+import { colors } from "common/colors";
+import { images } from "common";
 
 const Container = styled.div`
   height: 100%;
 `;
 
-const Header = styled.div`
-  margin-bottom: 20px;
+const TextContent = styled.div`
+
+`;
+
+const TextTopic = styled.h1`
+  background-color: ${colors.moderatePink};
+  text-align: center;
+  border-radius: 18px;
+`;
+
+const Text = styled.p`
+  background-color: ${colors.darkGray};
+  text-align: center;
+  font-size: x-large;
+  border-radius: 18px;
+  
+`;
+
+const Image = styled.div`
+  background-color: ${colors.darkGray};
+  text-align: center;
+  border-radius: 18px;
 `;
 
 const DashboardRoute = () => {
@@ -15,11 +37,55 @@ const DashboardRoute = () => {
   return (
     <Container>
       {authenticated ? (
-        <div>Jestem zalogowany</div>
+        <TextContent>
+          <TextTopic>THE TOURNAMENT4YOU APPLICATION</TextTopic>
+          <br></br>
+          <Text>
+          <br></br>
+          Hello, you have successfully logged into your account.
+            <br></br><br></br>
+          </Text>
+          <Image>
+            <img src={images.VoleyballPlayerLeft} alt="logo" />
+            <img src={images.VoleyballPlayerRight} alt="logo" />
+            </Image>
+          <Text>
+            This web application allows you to:
+            <br></br><br></br>
+            <b>Create a volleyball tournament.</b>
+            <br></br>
+            <b>Add the new teams to the tournament.</b>
+            <br></br>
+            <b>Add the players to the team.</b>
+            <br></br>
+            <b>Follow the tournament.</b>
+            <br></br><br></br>
+            </Text>
+        </TextContent>
+        
+        
       ) : (
-        <div>
-          <Header>Nie jestem zalogowany</Header>
-        </div>
+        <TextContent>
+          <TextTopic>WELCOME TO TOURNAMENT4YOU</TextTopic>
+          <br></br>
+          <Text>
+            <br></br>
+            Do you have a problem with managing volleyball tournaments? The <b>TOURNAMENT4YOU</b> application will surely solve it.
+            <br></br><br></br>
+            This web application is designed for people who want to set <b>voleyball tournaments</b>!
+            </Text>
+            <Image>
+              <img src={images.VoleyballTournamentLogo} alt="logo" />
+            </Image>
+            <Text>
+            Create a volleyball <b>event</b>, add <b>teams</b> to it, enter <b>players</b> and transparently follow the tournament, completing the results on a regular basis.
+            <br></br><br></br>
+            Thanks to the gameplay tree, following the tournament will become much simpler and more transparent.
+            <br></br><br></br>
+            Don't wait any longer! Create <b>an account</b> to be able to use the functionality of the application!
+            <br></br><br></br>
+          </Text>
+        </TextContent>
       )}
     </Container>
   );
