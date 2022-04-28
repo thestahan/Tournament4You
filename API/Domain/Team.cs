e14ebdc5f542abbf724158d9744d92e8224ce366
Kacper <thestahan@gmail.com>
2022-04-28 17:54:17 +0200

    92 - API - Add migration with missing organizerId

--- API/Domain/Team.cs
+++ API/Domain/Team.cs
@@ -6,6 +6,8 @@ public class Team
     public string Name { get; set; } = string.Empty;
     public string? City { get; set; }
     public string? Coach { get; set; }
+    public string OrganizerId { get; set; } = string.Empty;
+    public AppUser Organizer { get; set; } = default!;
     public ICollection<Tournament> Tournaments { get; set; } = new List<Tournament>();
     public ICollection<Player> Players { get; set; } = new List<Player>();
 }
