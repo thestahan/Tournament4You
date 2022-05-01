4839b34dd5a71c33ba278caf73c2271474a14763
Kacper <thestahan@gmail.com>
2022-05-01 10:01:26 +0200

    108 - API - Add migration with initial positions

---
+++ API/Data/Configs/PositionsConfig.cs
@@ -0,0 +1,45 @@
+﻿using API.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.Data.Configs;
+
+public class PositionsConfig : IEntityTypeConfiguration<Position>
+{
+    public void Configure(EntityTypeBuilder<Position> builder)
+    {
+        builder.HasData
+        (
+            new Position
+            {
+                Id = 1,
+                Name = "Outside hitter",
+                Abbreviation = "OH"
+            },
+            new Position
+            {
+                Id = 2,
+                Name = "Right-side hitter",
+                Abbreviation = "RH"
+            },
+            new Position
+            {
+                Id = 3,
+                Name = "Middle blocker",
+                Abbreviation = "MB"
+            },
+            new Position
+            {
+                Id = 4,
+                Name = "Setter",
+                Abbreviation = "S"
+            },
+            new Position
+            {
+                Id = 5,
+                Name = "Libero",
+                Abbreviation = "L"
+            }
+        );
+    }
+}
