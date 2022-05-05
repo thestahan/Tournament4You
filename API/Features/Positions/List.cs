8d5c41cf1738b0475b358fe9f042e75136ae2ff6
Kamil Eł <kamillancuchowski@gmail.com>
2022-05-05 12:05:56 +0200

    107 - API - Add feature to get list of postions

---
+++ API/Features/Positions/List.cs
@@ -0,0 +1,47 @@
+﻿using API.Data;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Features.Positions;
+
+public class List
+{
+    public record Query : IRequest<Result>
+    {
+    }
+
+    public class Result
+    {
+        public List<Position> Positions { get; init; } = new List<Position>();
+
+        public class Position
+        {
+            public int Id { get; set; }
+            public string Name { get; set; } = string.Empty;
+            public string Abbreviation { get; set; } = string.Empty;
+        }
+    }
+
+    public class Handler : IRequestHandler<Query, Result>
+    {
+        private readonly ApiDbContext _context;
+        private readonly IMapper _mapper;
+
+        public Handler(ApiDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Result> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var positions = await _context.Positions
+                .ProjectTo<Result.Position>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return new Result { Positions = positions };
+        }
+    }
+}
