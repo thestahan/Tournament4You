bae7c48ca9b066c3f792edec576dc48c667961c3
Maciej Kulinski <mafciej99@gmail.com>
2022-09-15 22:51:21 +0200

    feat(72) remove old dashboard and contact subpages views

--- frontend/src/contact/contact-route.tsx
+++ frontend/src/contact/contact-route.tsx
@@ -1,5 +1,4 @@
 import styled from "@emotion/styled";
-import { images } from "common";
 import { PageHeader } from "common/page-container";
 
 const Container = styled.div`
@@ -11,37 +10,12 @@ const ContactStatement = styled.div`
   width: 100%;
 `;
 
-const Text = styled.p`
-  text-align: center;
-  font-size: x-large;
-  margin-top: 50px;
-`;
-
-const Image = styled.div`
-  text-align: center;
-`;
-
 const ContactRoute = () => {
   return (
     <>
       <PageHeader>Contact</PageHeader>
       <Container>
-        <ContactStatement>
-          <Text>
-            Do you want to contact with us?
-            <br></br>
-            <br></br>
-            <Image>
-              <img src={images.ContactLogo} alt="logo" />
-            </Image>
-          </Text>
-
-          <Text>
-            <br></br>
-            Write us a message to the e-mail address{" "}
-            <b>tournament4you@gmail.com</b> and we will answer you!
-          </Text>
-        </ContactStatement>
+        <ContactStatement></ContactStatement>
       </Container>
     </>
   );
--- frontend/src/dashboard/dashboard-route.tsx
+++ frontend/src/dashboard/dashboard-route.tsx
@@ -1,30 +1,10 @@
 import styled from "@emotion/styled";
 import { isAuthenticated } from "common/utils/local-storage";
-import { colors } from "common/colors";
-import { images } from "common";
 import { PageHeader } from "common/page-container";
+import { AuthenticatedDashboard } from "./authenticated";
 
 const Container = styled.div`
   height: 100%;
-`;
-
-const TextContent = styled.div``;
-
-const TextTopic = styled.h1`
-  background-color: ${colors.moderatePink};
-  text-align: center;
-  border-radius: 18px;
-`;
-
-const Text = styled.p`
-  text-align: center;
-  font-size: x-large;
-  border-radius: 18px;
-`;
-
-const Image = styled.div`
-  text-align: center;
-  border-radius: 18px;
 `;
 
 const DashboardRoute = () => {
@@ -32,67 +12,11 @@ const DashboardRoute = () => {
 
   return (
     <Container>
+      <PageHeader>Dashboard</PageHeader>
       {authenticated ? (
-        <TextContent>
-          <PageHeader>Dashboard</PageHeader>
-          <br></br>
-          <Text>
-            <br></br>
-            Hello, you have successfully logged into your account.
-            <br></br>
-            <br></br>
-          </Text>
-          <Image>
-            <img src={images.VoleyballPlayerLeft} alt="logo" />
-            <img src={images.VoleyballPlayerRight} alt="logo" />
-          </Image>
-          <Text>
-            This web application allows you to:
-            <br></br>
-            <br></br>
-            <b>Create a volleyball tournament.</b>
-            <br></br>
-            <b>Add the new teams to the tournament.</b>
-            <br></br>
-            <b>Add the players to the team.</b>
-            <br></br>
-            <b>Follow the tournament.</b>
-            <br></br>
-            <br></br>
-          </Text>
-        </TextContent>
+        <AuthenticatedDashboard />
       ) : (
-        <TextContent>
-          <TextTopic>WELCOME TO TOURNAMENT4YOU</TextTopic>
-          <br></br>
-          <Text>
-            <br></br>
-            Do you have a problem with managing volleyball tournaments? The{" "}
-            <b>TOURNAMENT4YOU</b> application will surely solve it.
-            <br></br>
-            <br></br>
-            This web application is designed for people who want to set{" "}
-            <b>voleyball tournaments</b>!
-          </Text>
-          <Image>
-            <img src={images.VoleyballTournamentLogo} alt="logo" />
-          </Image>
-          <Text>
-            Create a volleyball <b>event</b>, add <b>teams</b> to it, enter{" "}
-            <b>players</b> and transparently follow the tournament, completing
-            the results on a regular basis.
-            <br></br>
-            <br></br>
-            Thanks to the gameplay tree, following the tournament will become
-            much simpler and more transparent.
-            <br></br>
-            <br></br>
-            Don't wait any longer! Create <b>an account</b> to be able to use
-            the functionality of the application!
-            <br></br>
-            <br></br>
-          </Text>
-        </TextContent>
+        <div>Not authenticated</div>
       )}
     </Container>
   );
